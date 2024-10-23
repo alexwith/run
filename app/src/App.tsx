e979c7cabc236d80aace476ffcc3ce302cceeae6
Alex <alex@with.priv.no>
2024-10-23 15:32:34 +0200

    feat: start on terminal

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,17 +1,17 @@
 import { RunIcon } from "./common/icons";
-import { CodeEditor } from "./components/CodeEditor";
+import CodeEditor from "./components/CodeEditor";
+import Terminal from "./components/Terminal";
 
 function App() {
   return (
     <>
-      <div className="w-screen h-screen flex justify-center items-center">
-        <div>
-          <div className="my-2">
-            <div className="flex items-center gap-1 bg-sky-500 text-white font-bold w-fit px-2 rounded-md hover:cursor-pointer">
-              <span>Run</span> <RunIcon size={14} />
-            </div>
+      <div className="w-screen h-screen flex justify-center">
+        <div className="flex flex-col gap-2 mt-[30%]">
+          <div className="flex items-center gap-1 bg-sky-500 text-white font-bold w-fit px-2 rounded-md hover:cursor-pointer">
+            <span>Run</span> <RunIcon size={14} />
           </div>
           <CodeEditor />
+          <Terminal />
         </div>
       </div>
     </>
---
+++ app/src/components/Terminal.tsx
@@ -0,0 +1,7 @@
+export default function Terminal() {
+  return (
+    <div className="border-[1px] border-zinc-800 rounded-lg overflow-hidden bg-[#1A1B26] text-white text-xs p-2 font-mono">
+      Hello there
+    </div>
+  );
+}
