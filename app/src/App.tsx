9be8718a7d95f357a45d2f5390055231a53f659a
Alex <alex@with.priv.no>
2024-10-29 15:53:20 +0100

    feat: add title to website

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,8 +5,21 @@ import Terminal from "./components/Terminal";
 function App() {
   return (
     <>
-      <div className="w-screen h-screen flex justify-center">
-        <div className="flex flex-col gap-2 mt-[30%]">
+      <div className="w-screen h-screen flex items-center flex-col">
+        <div className="m-10">
+          <div className="flex items-center justify-center">
+            <span className="absolute mx-auto py-4 flex border w-fit bg-sky-400 blur-xl bg-clip-text text-6xl box-content font-extrabold text-transparent text-center select-none">
+              Run
+            </span>
+            <h1 className="relative top-0 w-fit h-auto py-4 justify-center flex bg-sky-400 items-center bg-clip-text text-6xl font-extrabold text-transparent text-center select-auto">
+              Run
+            </h1>
+          </div>
+          <p className="text-zinc-400 font-semibold text-xl">
+            Execute code quickly in the browser!
+          </p>
+        </div>
+        <div className="flex flex-col gap-2 w-fit">
           <MenuBar />
           <CodeEditor />
           <Terminal />
