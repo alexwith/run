8fd07f18e29c301deedc84283c0bbb333e204cd9
Alex <alex@with.priv.no>
2024-10-30 09:37:24 +0100

    refactor: app -> frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,11 +9,11 @@ function App() {
         <div className="m-10">
           <div className="flex items-center justify-center">
             <span className="absolute mx-auto py-4 flex items-center border w-fit bg-sky-400 blur-xl bg-clip-text text-6xl box-content font-extrabold text-transparent text-center select-none">
-              <img className="w-16" src="public/logo.svg" />
+              <img className="w-16" src="/logo.svg" />
               <span>Run</span>
             </span>
             <h1 className="relative top-0 w-fit h-auto py-4 justify-center flex bg-sky-400 items-center bg-clip-text text-6xl font-extrabold text-transparent text-center select-auto">
-              <img className="w-16" src="public/logo.svg" />
+              <img className="w-16" src="/logo.svg" />
               <span>Run</span>
             </h1>
           </div>
