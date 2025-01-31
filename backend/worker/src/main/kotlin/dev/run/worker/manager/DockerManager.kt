b274e095bd14b2ede641bef2423a4dae00f05bd7
Alex <alex@with.priv.no>
2025-01-31 19:03:28 +0100

    fix: line breaks in code would fail to compile and increase memory limit

--- backend/worker/src/main/kotlin/dev/run/worker/manager/DockerManager.kt
+++ backend/worker/src/main/kotlin/dev/run/worker/manager/DockerManager.kt
@@ -96,6 +96,6 @@ class DockerManager {
     companion object {
         private const val TIMEOUT: Int = 8 // seconds
         private const val PROCESS_LIMIT: Int = 1 // processes
-        private const val MEMORY_LIMIT: String = "10m"
+        private const val MEMORY_LIMIT: String = "30m"
     }
 }
